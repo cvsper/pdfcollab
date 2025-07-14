3afc0123c971fcabf8f55012ffb4bbd780cfa714
shamar donaldson <sevs@macbookpro.mynetworksettings.com>
2025-07-14 01:50:54 -0400

    Revert "Completely eliminate base64 data from PDF form fields"
    This reverts commit ae1f085025f7455750bd6e761c6f40eaf1f8ed7b.

--- pdf_processor.py
+++ pdf_processor.py
@@ -848,11 +848,8 @@ class PDFProcessor:
                         
                         # Track signature fields separately
                         if field.get('type') == 'signature':
-                            # For image signatures, use empty value to prevent base64 in form
-                            signature_value = "" if field.get('is_image_signature', False) else field['value']
-                            
                             signature_fields[pdf_field_name] = {
-                                'value': signature_value,
+                                'value': field['value'],
                                 'position': field.get('position', {}),
                                 'page': field.get('page', 0),
                                 'name': field.get('name', ''),
